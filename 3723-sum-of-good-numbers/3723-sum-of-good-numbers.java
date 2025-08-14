cfbe52cb3c2e6b008f5636b3a6b773a65f6519f8
ARVIND M <126601502+Arvind22cse@users.noreply.github.com>
2025-08-14 13:47:24 +0530

    Time: 1 ms (100%), Space: 44.2 MB (63.88%) - LeetHub

---
+++ 3723-sum-of-good-numbers/3723-sum-of-good-numbers.java
@@ -0,0 +1,41 @@
+class Solution {
+    public int sumOfGoodNumbers(int[] nums, int k) {
+        /*
+             1 3 2 1 5 4
+             nums[0+2]=2
+             2<1 
+
+             nums[1+2]=1 
+             3>1 && nums[i-2]=-1 3>-1 s+=3
+
+             nums[2+2]=4
+             2>
+
+             nums[3+2]
+
+
+        */
+        int n=nums.length;
+        int s=0;
+        for (int i=0;i<n;i++){
+            if(i-k<0){
+                if(nums[i+k]<nums[i] ){
+                    s+=nums[i];
+                }   
+            }
+            if((i+k)>n-1){
+                if(nums[i-k]<nums[i] ){
+                    s+=nums[i];
+                }
+                
+            }
+            // if((nums[i+k]<nums[i])&& (nums[i-k]<nums[i]) && (i+k)<n && (i-k)>=0){
+                if (i + k < n && i - k >= 0 &&
+                nums[i + k] < nums[i] && nums[i - k] < nums[i]) { 
+                s+=nums[i];
+                
+            }
+        }
+        return s;
+    }
+}
