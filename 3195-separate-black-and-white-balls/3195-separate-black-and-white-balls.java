2d57174e86658b51238618f036c465b12c5457ba
ARVIND M <126601502+Arvind22cse@users.noreply.github.com>
2025-08-25 14:58:08 +0530

    Time: 8 ms (33.27%), Space: 45.5 MB (55.38%) - LeetHub

--- 3195-separate-black-and-white-balls/3195-separate-black-and-white-balls.java
+++ 3195-separate-black-and-white-balls/3195-separate-black-and-white-balls.java
@@ -1,15 +1,15 @@
 class Solution {
     public long minimumSteps(String s) {
-        int p1=0,p2;
+        int p1=0,p2=s.length()-1;
         long c=0;
-        while(p1<s.length()&&s.charAt(p1)=='0') p1++;
-        p2=p1+1;
-        while(p2<s.length()) {
-            if(s.charAt(p2)=='0') {
+        while(p1<p2) {
+            if(s.charAt(p1)=='0') p1++;
+            else if(s.charAt(p2)=='1') p2--;
+            else {
                 c+=(p2-p1);
                 p1++;
+                p2--;
             }
-            p2++;
         }
         return c;
     }
