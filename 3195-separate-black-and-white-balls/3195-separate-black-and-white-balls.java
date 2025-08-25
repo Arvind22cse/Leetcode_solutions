c7004c8a71f76f7a35ce6f498b3a54ad62cb2180
ARVIND M <126601502+Arvind22cse@users.noreply.github.com>
2025-08-25 14:54:02 +0530

    Time: 8 ms (33.27%), Space: 45.8 MB (14.04%) - LeetHub

--- 3195-separate-black-and-white-balls/3195-separate-black-and-white-balls.java
+++ 3195-separate-black-and-white-balls/3195-separate-black-and-white-balls.java
@@ -1,36 +1,15 @@
 class Solution {
     public long minimumSteps(String s) {
-        char[] arr=s.toCharArray();
-        int[] arr2=new int[arr.length];
-        for(int i=0;i<arr.length;i++){
-            arr2[i]=arr[i]-'0';
-        }
+        int p1=0,p2;
         long c=0;
-        // for(int i=0;i<arr2.length;i++){
-        //     for(int j=0;j<arr2.length-1-i;j++){
-        //         if(arr2[j]>arr2[j+1]){
-        //             int t=arr2[j];
-        //             arr2[j]=arr2[j+1];
-        //             arr2[j+1]=t;
-        //             c++;
-        //         }
-        //     }
-        // }
-        int l=0;
-        int r=arr2.length-1;
-        while(l<r){
-            if(arr2[l]==0){
-                l++;
+        while(p1<s.length()&&s.charAt(p1)=='0') p1++;
+        p2=p1+1;
+        while(p2<s.length()) {
+            if(s.charAt(p2)=='0') {
+                c+=(p2-p1);
+                p1++;
             }
-            else if(arr2[r]==1){
-                r--;
-            }
-            else{
-                
-                c+=(r-l);
-                l++;
-                r--;
-            }
+            p2++;
         }
         return c;
     }
