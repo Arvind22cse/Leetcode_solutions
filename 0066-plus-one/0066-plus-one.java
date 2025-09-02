ba1368ebb8e73572f0772f8f2d523f4044749602
ARVIND M <126601502+Arvind22cse@users.noreply.github.com>
2025-09-02 09:32:46 +0530

    Time: 1 ms (5.22%), Space: 42.3 MB (9.54%) - LeetHub

--- 0066-plus-one/0066-plus-one.java
+++ 0066-plus-one/0066-plus-one.java
@@ -1,14 +1,31 @@
 class Solution {
     public int[] plusOne(int[] digits) {
-        for(int i=digits.length-1;i>=0;i--) {
-            if(digits[i]<9) {
-                digits[i]++;
-                return digits;
-            }
-            digits[i]=0;
+           StringBuilder sb = new StringBuilder();
+        for (int d : digits) {
+            sb.append(d);
         }
-        int[] arr=new int[digits.length+1];
-        arr[0]=1;
+        String str = sb.toString();
+
+        int i = str.length() - 1;
+        int carry = 1; // since we want +1
+        StringBuilder str2 = new StringBuilder();
+
+        while (i >= 0 || carry > 0) {
+            int a = (i >= 0) ? str.charAt(i) - '0' : 0;
+            int sum = a + carry;
+
+            str2.append(sum % 10);
+            carry = sum / 10;
+            i--;
+        }
+
+        str2.reverse(); // because we built from last digit
+
+        int n = str2.length();
+        int[] arr = new int[n];
+        for (int j = 0; j < n; j++) {
+            arr[j] = str2.charAt(j) - '0';
+        }
         return arr;
     }
 }
